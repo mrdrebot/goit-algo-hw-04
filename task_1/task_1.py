4aef92feb660e4724b02a71a4f76c825f9e26434
mrdrebot <m.r.drebot.j@gmail.com>
2024-10-17 23:06:53 +0300

    Corrected the code according to the comments of the mentor

--- task_1/task_1.py
+++ task_1/task_1.py
@@ -1,25 +1,29 @@
 from pathlib import Path
 
+
 def total_salary(path: Path) -> tuple[int, int]:
-    with open(path, 'r', encoding = 'utf-8') as file:
-        users_list: list = [el.strip().split(",") for el in file.readlines()]
+    try:
+        with open(path, 'r', encoding = 'utf-8') as file:
+            users_list: list = [el.strip().split(",") for el in file.readlines()]
 
-        if not users_list:
-            return ()
+            if not users_list:
+                print("У файлі відсутні данні! Перевірте файл!")
+                return (0, 0)
 
-        users_dic: dict = {key: int(value) for key, value in users_list}
-        total: int = sum(users_dic.values())
-        average: int = total // len(users_dic)
-        return total, average
+            users_dic: dict = {key: int(value) for key, value in users_list}
+            total: float = sum(users_dic.values())
+            average: float = total / len(users_dic)
+            return total, average
+    except FileNotFoundError:
+        print("Програма не змогла знайти файл!")
+        return (0,0)
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+        return (0,0)
+    finally:
+        print("Якщо хочете повторити роботу програми, запустіть її заново!")
     
-file_path: Path = Path(Path.cwd(), "task_1_data_1.txt")
+file_path: Path = Path(Path.cwd(), "task_1_data_4.txt")
 
-try:
-    total, average = total_salary(file_path)
-    print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")
-except FileNotFoundError:
-    print("Програма не змогла знайти файл!")
-except (IndexError, ValueError):
-    print("Щось не так і даними у файлі! Перевірте дані у файлі!")
-finally:
-    print("Якщо хочете повторити роботу програми, запустіть її заново!")
+total, average = total_salary(file_path)
+print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")
--- task_2/task_2.py
+++ task_2/task_2.py
@@ -1,30 +1,26 @@
 from pathlib import Path 
 
-def get_cats_info(path: Path) -> dict:
+def get_cats_info(path: Path) -> list:
     keys_list: list = ["id", "name", "age"]
-    with open(path, 'r') as file:
-        cats_list: list = [str.strip().split(",") for str in file.readlines()]
-        cats_dict: dict = [dict(zip(keys_list, cat)) for cat in cats_list]
-        return cats_dict
+    try:
+        with open(path, 'r', encoding = 'utf-8') as file:
+            cats_list: list = [str.strip().split(",") for str in file.readlines()]
+            print(cats_list)
+            
+            for cat in cats_list:
+                if len(cat) != 3:
+                    print("У файлі відсутні або некоректні данні!")
+                    return []
+            
+            cats_list_with_keys: list = [dict(zip(keys_list, cat)) for cat in cats_list]
+            return cats_list_with_keys
+    except FileNotFoundError:
+        print("Програма не змогла знайти файл!")
+        return []
+    finally:
+        print("Якщо хочете повторити роботу програми, запустіть її заново!")
 
 file_name: str = "task_2_data_3.txt"
 
-try:
-    cats_info: dict = get_cats_info(file_name)
-    print(cats_info)
-except FileNotFoundError:
-    print("Програма не змогла знайти файл!")
-except (IndexError, ValueError):
-    print("Щось не так і даними у файлі! Перевірте дані у файлі!")
-finally:
-    print("Якщо хочете повторити роботу програми, запустіть її заново!")
-
-'''
-[
-    {"id": "60b90c1c13067a15887e1ae1", "name": "Tayson", "age": "3"},
-    {"id": "60b90c2413067a15887e1ae2", "name": "Vika", "age": "1"},
-    {"id": "60b90c2e13067a15887e1ae3", "name": "Barsik", "age": "2"},
-    {"id": "60b90c3b13067a15887e1ae4", "name": "Simon", "age": "12"},
-    {"id": "60b90c4613067a15887e1ae5", "name": "Tessi", "age": "5"},
-]
-'''
+cats_info: list = get_cats_info(file_name)
+print(cats_info)
